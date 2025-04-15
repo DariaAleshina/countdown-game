4a6f521470362ce359bb0c2e7bdbd6265ca09168
DariaAleshina <ddaleshina@gmail.com>
2025-04-15 17:16:47 +0200

    seperated timer content in a seperate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
+import TIMER_CHALLENGES_CONTENT from './components/TimerChallangesContent.js';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        {TIMER_CHALLENGES_CONTENT.map(({ title, targetTime }) => (
+          <TimerChallenge title={title} targetTime={targetTime} />
+        ))}
+      </div>
     </>
   );
 }
---
+++ src/components/TimerChallangesContent.js
@@ -0,0 +1,20 @@
+const TIMER_CHALLENGES_CONTENT = [
+    {
+        title: "Easy",
+        targetTime: 1,
+    },
+    {
+        title: "Not Easy",
+        targetTime: 5,
+    },
+    {
+        title: "Getting Tough",
+        targetTime: 10,
+    },
+    {
+        title: "Not Easy",
+        targetTime: 15,
+    },
+]
+
+export default TIMER_CHALLENGES_CONTENT;
